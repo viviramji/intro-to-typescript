4437761a505140fc280e513a2a79f57e12725f5f
Victor Victor RJ <wrjr04@gmail.com>
2023-08-20 15:51:17 -0500

    Fix test ts.

--- examples/express-api/src/api/todos/todo.test.ts
+++ examples/express-api/src/api/todos/todo.test.ts
@@ -2,6 +2,14 @@ import request from 'supertest';
 
 import app from '../../app';
 
+import { Todos } from './todo.model';
+
+beforeAll(async () => {
+  try {
+    await Todos.drop();
+  } catch (error) { }
+});
+
 describe('GET /api/v1/todos', () => {
   it('responds with a todos list', async () => {
     request(app)
@@ -9,11 +17,9 @@ describe('GET /api/v1/todos', () => {
       .set('Accept', 'application/json')
       .expect('Content-Type', /json/)
       .expect(200)
-      .then((response) =>{
+      .then((response) => {
         expect(response.body).toHaveProperty('length');
-        expect(response.body.length).toBe(1);
-        expect(response.body[0]).toHaveProperty('content');
-        expect(response.body[0]).toHaveProperty('done');
+        expect(response.body.length).toBe(0);
       });
   });
 });
