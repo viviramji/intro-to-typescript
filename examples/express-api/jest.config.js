47ceac682932141b0377eda1c8056d154c283f8a
Victor Victor RJ <wrjr04@gmail.com>
2023-08-21 14:40:01 -0500

    Firx warning closing db.

--- examples/express-api/jest.config.js
+++ examples/express-api/jest.config.js
@@ -2,6 +2,6 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  // setupFilesAfterEnv: ['<rootDir>/src/setupFilesAfterEnv.ts'],
+  setupFilesAfterEnv: ['<rootDir>/src/setupFilesAfterEnv.ts'],
   modulePathIgnorePatterns: ['<rootDir>/dist/'],
 };
